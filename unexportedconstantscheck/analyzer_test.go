9ae06a89f23de3c9a69d70d5ebd3292a524cdf83
Manuel Doncel Martos <manueldoncelmartos@gmail.com>
2025-11-26 14:49:05 +0100

    chore: small improvements

--- unexportedconstantscheck/analyzer_test.go
+++ unexportedconstantscheck/analyzer_test.go
@@ -15,12 +15,34 @@ func TestAnalyzer(t *testing.T) {
 			desc:     "default",
 			patterns: "simple",
 		},
-		/* TODO(manuelarte): second iteration
 		{
 			desc:     "complex",
 			patterns: "complex",
 		},
-		*/
+	}
+
+	for _, test := range testCases {
+		t.Run(test.desc, func(t *testing.T) {
+			a := NewAnalyzer()
+
+			analysistest.Run(t, analysistest.TestData(), a, test.patterns)
+		})
+	}
+}
+
+func TestAnalyzerWithSuggestedFixes(t *testing.T) {
+	testCases := []struct {
+		desc     string
+		patterns string
+	}{
+		{
+			desc:     "default",
+			patterns: "simple-fix",
+		},
+		{
+			desc:     "complex",
+			patterns: "complex-fix",
+		},
 	}
 
 	for _, test := range testCases {
