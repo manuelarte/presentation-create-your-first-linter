65a22ffb5b8b5464054ed43be2ddf725ec804f9a
manuelarte <manueldoncelmartos@gmail.com>
2026-01-28 16:13:27 +0100

    chore: using ast.Print

--- astexample/main.go
+++ astexample/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"go/ast"
 
 	"golang.org/x/tools/go/analysis"
@@ -24,23 +23,7 @@ func NewAnalyzer() *analysis.Analyzer {
 
 func run(pass *analysis.Pass) (any, error) {
 	for _, file := range pass.Files {
-		ast.Inspect(file, func(n ast.Node) bool {
-			switch casted := n.(type) {
-			case *ast.File:
-				fmt.Printf("ast.File: %q\n", casted.Name.Name)
-			case *ast.Ident:
-				fmt.Printf("ast.Ident: %q\n", casted.Name)
-			case *ast.ImportSpec:
-				fmt.Printf("ast.ImportSpec: %q\n", casted.Path.Value)
-			case *ast.ValueSpec:
-				fmt.Printf("ast.ValueSpec: %q\n", casted.Names)
-			case *ast.FuncDecl:
-				fmt.Printf("FuncDecl: %q Params(%d) Returns(%d)\n", casted.Name.Name, len(casted.Type.Params.List), len(casted.Type.Results.List))
-			default:
-				fmt.Printf("%#+v\n", casted)
-			}
-			return true
-		})
+		_ = ast.Print(pass.Fset, file)
 	}
 	return nil, nil
 }
